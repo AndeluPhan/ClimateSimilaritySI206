eacbfad2b3ee017d7ec1e4e3f5c2dea959ec1e44
trianna <66096388+triannan@users.noreply.github.com>
2023-04-17 00:48:54 -0400

    added calculation and 2 plots

--- CityClimates.py
+++ CityClimates.py
@@ -1,12 +1,12 @@
 import sqlite3
 import numpy as np
 from numpy.linalg import norm
+import matplotlib.pyplot as plt
+from math import sin, cos, sqrt, atan2, radians
 
-def calculateRankings(chosen_city):
+def calculateRankings(cur, conn, chosen_city):
     # 1 cosine similarity represents 1 month. 
     # generate 12 cosine similarities, average and sort. 
-    conn = sqlite3.connect("climateProject.db")
-    cur = conn.cursor()
     cur.execute("SELECT city_id FROM population WHERE city == ?", (chosen_city, ))
     chosen_city_id = cur.fetchone()[0]
     cur.execute("SELECT * FROM climateData WHERE city_id == ?", (chosen_city_id, ))
@@ -34,11 +34,83 @@ def calculateRankings(chosen_city):
     
     f.write(f"\nCity with the most similar climate to {chosen_city}: {sorted_similarities[0][0]}\n")
     f.write(f"City with the least similar climate to {chosen_city}: {sorted_similarities[-1][0]}\n")
+    f.close()
+
+    # plotting the top 10 cities with the most similar climate
+    city_name = [i[0] for i in sorted_similarities[:10]]
+    city_cosine_similarity = [i[1] for i in sorted_similarities[:10]]
+    plt.barh(city_name, city_cosine_similarity, color="darkorange")
+    plt.xlabel(f"Climate Cosine Similarity to {chosen_city}")
+    plt.ylabel("Cities")
+    plt.xlim(sorted_similarities[10][1], 1.0000)
+    plt.title(f"Top 10 Cities With Similar Climate To {chosen_city}")
+    plt.gcf().subplots_adjust(left=0.25)
+    plt.savefig("topTenSimilarClimates.png")
+
+    return cosine_similarities
+
+def calculateSimilaritiesByLatLon(cur, conn, city, cosine_similarities):
+    cur.execute("SELECT city, lat, lon FROM population WHERE city == ?", (city, ))
+    chosen_city_lat_lon = cur.fetchone()
+    cur.execute("SELECT city, lat, lon FROM population WHERE city != ?", (city, ))
+    city_lat_lon = cur.fetchall()
+    latitude_diff = {}
+    longitude_diff = {}
+    lat1 = chosen_city_lat_lon[1]
+    lon1 = chosen_city_lat_lon[2]
+    for cities in city_lat_lon:
+        lat2 = cities[1]
+        lon2 = cities[2]
+        lat_diff = abs(lat1 - lat2)
+        lon_diff = abs(lon1 - lon2)
+        latitude_diff[cities[0]] = lat_diff
+        longitude_diff[cities[0]] = lon_diff
+    sorted_lat_diff = sorted(latitude_diff.items(), key=lambda x:x[1])
+    sorted_lon_diff = sorted(longitude_diff.items(), key=lambda x:x[1])
+
+    f = open("calculated_data.txt", 'a')
+    f.write(f"\nCities with the closest latitude to {city}:\n")
+    for cities in sorted_lat_diff:
+        f.write(f"{cities[0]}, ")
+    
+    f.write(f"\n\nCities with the closest longitude to {city}:\n")
+    for cities in sorted_lon_diff:
+        f.write(f"{cities[0]}, ")
+    f.close()
+
+    # plotting the lat and lon vs climate similarity
+    x_axis1 = []
+    y_axis1 = []
+    for dist in sorted_lat_diff:
+        x_axis1.append(dist[1])
+        y_axis1.append(cosine_similarities[dist[0]])
+    x_axis2 = []
+    y_axis2 = []
+    for dist in sorted_lon_diff:
+        x_axis2.append(dist[1])
+        y_axis2.append(cosine_similarities[dist[0]])
+    fig, (ax1, ax2) = plt.subplots(2)
+    # plotting lat
+    ax1.plot(x_axis1, y_axis1, 'hotpink', label="Latitude")
+    ax1.set(xlabel="Latitude Difference", ylabel="Climate Cosine Similarity", title=f"Latitude Difference VS Climate Cosine Similarity ({city})")
+    ax1.set_xlim(0, 80)
+    ax1.grid()
+    # plotting lon
+    ax2.plot(x_axis2, y_axis2, 'lightsteelblue', label="Longitude")
+    ax2.set(xlabel="Longitude Difference", ylabel="Climate Cosine Similarity", title=f"Longitude Difference VS Climate Cosine Similarity ({city})")
+    ax2.set_xlim(0, 80)
+    ax2.grid()
+    fig.tight_layout()
+    fig.savefig("similarityLatLonPlot.png")
+        
 
 
 def main():
+    conn = sqlite3.connect("climateProject.db")
+    cur = conn.cursor()
     city = input("Enter a city: ")
-    calculateRankings(city)
+    cosine_similarities = calculateRankings(cur, conn, city)
+    calculateSimilaritiesByLatLon(cur, conn, city, cosine_similarities)
 
 
 if __name__ == '__main__':
